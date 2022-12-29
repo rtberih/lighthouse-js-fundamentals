7cb0f9c71d7329999aceaebf07b636d49a060a66
rtberih <radwan.berih1@gmail.com>
2022-12-29 10:26:58 -0500

    iterating over arrays, writing different forms of for loops

---
+++ ingredients.js
@@ -0,0 +1,16 @@
+// While loop
+let i = 0;
+while (i < ingredients.length) {
+  console.log(ingredients[i]);
+  i++;
+}
+
+// For loop
+for (let i = 0; i < ingredients.length; i++) {
+  console.log(ingredients[i]);
+}
+
+// Loop that prints ingredients backwards
+for (let i = ingredients.length - 1; i >= 0; i--) {
+  console.log(ingredients[i]);
+}
